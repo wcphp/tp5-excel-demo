71cc5066af0e6f92eed2f80af1cb7cf08da77a9b
weika <ka.wei@diandian7.com>
2019-05-16 09:41:46 +0800

    繁体简体首字母导入   行业职位导入

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -1,15 +1,92 @@
 <?php
 namespace app\index\controller;
 
+//use PhpOffice\PhpSpreadsheet\IOFactory;
+use PhpOffice\PhpSpreadsheet\IOFactory;
+use PhpOffice\PhpSpreadsheet\Spreadsheet;
+use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
+use think\Db;
+
 class Index
 {
     public function index()
     {
-        return '<style type="text/css">*{ padding: 0; margin: 0; } div{ padding: 4px 48px;} a{color:#2E5CD5;cursor: pointer;text-decoration: none} a:hover{text-decoration:underline; } body{ background: #fff; font-family: "Century Gothic","Microsoft yahei"; color: #333;font-size:18px;} h1{ font-size: 100px; font-weight: normal; margin-bottom: 12px; } p{ line-height: 1.6em; font-size: 42px }</style><div style="padding: 24px 48px;"> <h1>:) </h1><p> ThinkPHP V5.1<br/><span style="font-size:30px">12载初心不改（2006-2018） - 你值得信赖的PHP框架</span></p></div><script type="text/javascript" src="https://tajs.qq.com/stats?sId=64890268" charset="UTF-8"></script><script type="text/javascript" src="https://e.topthink.com/Public/static/client.js"></script><think id="eab4b9f840753f8e7"></think>';
+        //写
+//        $spreadsheet = new Spreadsheet();
+//        $sheet = $spreadsheet->getActiveSheet();
+//        $sheet->setCellValue('A1', 'Welcome to Helloweba.');
+//
+//        $writer = new Xlsx($spreadsheet);
+//        dump($writer->save('hello.xlsx'));
+
+
+
+
+
+    }
+    /**
+     * 首字母
+     * @return
+     * @throws \Exception
+     */
+    public function firstZM()
+    {
+        $inputFileName = './data/jian.xls';
+        $spreadsheet = IOFactory::load($inputFileName);
+        $sheetData = $spreadsheet->getActiveSheet()->toArray(null, true, true, true);
+        $arr = [];
+        foreach($sheetData as $item){
+
+         if(!isset($arr[$item['A']])){
+                $arr[$item['A']] =  $item['B'];
+            }
+        }
+
+        $inputFileName1 = './data/fan.xls';
+        $spreadsheet1 = IOFactory::load($inputFileName1);
+        $sheetData1 = $spreadsheet1->getActiveSheet()->toArray(null, true, true, true);
+
+        foreach($sheetData1 as $item){
+
+            if(!isset($arr[$item['A']])){
+                $arr[$item['A']] =  $item['B'];
+            }
+        }
+        echo json_encode($arr,JSON_UNESCAPED_UNICODE);
+
     }
 
-    public function hello($name = 'ThinkPHP5')
+    /**
+     * 行业
+     * @return
+     * @throws \Exception
+     */
+    public function hangye()
     {
-        return 'hello,' . $name;
+        $inputFileName = './data/hangye.xlsx';
+        $spreadsheet = IOFactory::load($inputFileName);
+        $sheetData = $spreadsheet->getActiveSheet()->toArray(null, true, true, true);
+        $id = 1;
+        $pid = 0;
+        foreach($sheetData as $item){
+            if(!empty($item['A'])){
+                Db::name('industry')->insert(['id'=>$id,'pid'=>0,'name'=>$item['A']]);
+                $pid = $id;
+                $id++;
+            }
+            if(!empty($item['B']) && trim($item['B']) !='其他'){
+                Db::name('industry')->insert(['id'=>$id,'pid'=>$pid,'name'=>$item['B']]);
+                $id++;
+            }
+
+        }
+
     }
+
+
+
+
+
+
+
 }
